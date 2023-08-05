ea0dd1c64d9b0d5f94c670f3b684462768f77643
Albert Ejuku <albertejuku92@gmail.com>
2023-08-05 16:35:26 +0300

    Initial commit

---
+++ src/main/java/com/house_of_dahlia/HouseOfDahliaApplication.java
@@ -0,0 +1,13 @@
+package com.house_of_dahlia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseOfDahliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseOfDahliaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/house_of_dahlia/HouseOfDahliaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.house_of_dahlia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseOfDahliaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
